d7765da5ff5bd0d8c74e1e463f5f0cd528ec838a
devchandhana12 <chandana.n@ascent-hr.com>
2024-06-05 11:54:23 +0530

    jest setup

---
+++ lib/components/Button/button.test.tsx
@@ -0,0 +1,17 @@
+import { render, fireEvent } from "@testing-library/react";
+import Button from "./index";
+
+describe("Button component", () => {
+  it("renders with the correct label", () => {
+    const label = "Click me!";
+    const { getByText } = render(<Button label={label} />);
+    expect(getByText(label)).toBeDefined();
+  });
+
+  it("displays an alert when clicked", () => {
+    const { getByRole } = render(<Button label="Click me!" />);
+    const button = getByRole("button");
+    fireEvent.click(button);
+    expect(window.alert).toBeDefined();
+  });
+});
