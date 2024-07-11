d3445a526c2fdc06b98e53fe86caf642d330673d
Jayaram Akula <akhulaghanasai@ascent-hr.com>
2024-07-11 15:40:56 +0530

    added styles in Table component

--- lib/components/Table/index.tsx
+++ lib/components/Table/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import DataTable from "react-data-table-component";
 import { AnyObject } from "yup";
+import "../../assets/table.css";
 
 interface tableProps {
   data: AnyObject[];
@@ -10,7 +11,7 @@ const Table: React.FC<tableProps> = ({ data, columns }) => {
   return (
     <div>
       <p>tabling is gonna come soon!!!</p>
-      <DataTable data={data} columns={columns} />
+      <DataTable data={data} columns={columns} pagination />
     </div>
   );
 };
