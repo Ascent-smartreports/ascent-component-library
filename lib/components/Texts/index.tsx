a21f167cd5b18140a093d09709e9fd45b8bfe890
devchandhana12 <chandana.n@ascent-hr.com>
2024-07-23 12:06:24 +0530

    onclick handle for texts

--- lib/components/Texts/index.tsx
+++ lib/components/Texts/index.tsx
@@ -5,38 +5,64 @@ interface TextProps {
   children: React.ReactNode;
   className?: string;
   type?: "error";
+  onTextClick?: () => void;
 }
 
-export const Heading: React.FC<TextProps> = ({ children, className }) => {
+export const Heading: React.FC<TextProps> = ({
+  children,
+  className,
+  onTextClick,
+}) => {
   return (
-    <h2 className={className || styles.heading} style={{ fontWeight: "bold" }}>
+    <h2
+      className={className || styles.heading}
+      style={{ fontWeight: "bold" }}
+      onClick={() => onTextClick}
+    >
       {children}
     </h2>
   );
 };
 
-export const SubHeading: React.FC<TextProps> = ({ children, className }) => {
+export const SubHeading: React.FC<TextProps> = ({
+  children,
+  className,
+  onTextClick,
+}) => {
   return (
     <h4
       className={className || styles.subHeading}
       style={{ fontWeight: "bold" }}
+      onClick={() => onTextClick}
     >
       {children}
     </h4>
   );
 };
 
-export const Label: React.FC<TextProps> = ({ children, className }) => {
-  return <h6 className={className || styles.label}>{children}</h6>;
+export const Label: React.FC<TextProps> = ({
+  children,
+  className,
+  onTextClick,
+}) => {
+  return (
+    <h6 className={className || styles.label} onClick={() => onTextClick}>
+      {children}
+    </h6>
+  );
 };
 
 export const Paragraph: React.FC<TextProps> = ({
   children,
   className,
   type,
+  onTextClick,
 }) => {
   return (
-    <p className={className || type ? styles.errorText : styles.paragraph}>
+    <p
+      className={className || type ? styles.errorText : styles.paragraph}
+      onClick={() => onTextClick}
+    >
       {children}
     </p>
   );
